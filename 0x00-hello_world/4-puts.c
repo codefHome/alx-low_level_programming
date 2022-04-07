7d557d53e8ad8eee959f8c6f40796ba61b78408d
Code@Home <bedada@gmail.com>
2022-04-07 04:31:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,19 @@
+/*
+ * File:4-puts.c
+ * Auth: Bedada Bekele 
+ */
+
 #include<stdio.h>
+
+/**
+ * main -Prints "Programming is like building a multilingual
+ * 	 puzzle,followed by a new line.
+ *
+ * 	 Return:Always 0.
+ */
+
+
 int main(void){
-	puts("\nProgramming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
