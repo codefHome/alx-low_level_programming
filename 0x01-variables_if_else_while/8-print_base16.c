81d2f466897591b9f30744efa2fe81a6b9e4e113
Code@Home <bedada@gmail.com>
2022-04-08 06:16:33 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -17,7 +17,7 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar(num % 10 + '0');
-	for (letter = 'A'; letter <= 'E'; letter++)
+	for (letter = 'a'; letter <= 'e'; letter++)
 		putchar(letter);
 
 	putchar('\n');
