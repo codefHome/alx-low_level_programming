815ee35dc64b382bd89677610a302217886a442a
Code@Home <bedada@gmail.com>
2022-04-13 12:07:32 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 	int x = 1;
 	int y = 2;
 	int sum, i;
-	
-	printf("%d, %d, ", x,y);
+
+	printf("%d, %d, ", x, y);
 	for (i = 0; i <= 98; i++)
 	{
 		sum = x + y;
-		printf("%d,' '", sum);
+		printf("%d, ", sum);
 		x = y;
 		y = sum;
 		
