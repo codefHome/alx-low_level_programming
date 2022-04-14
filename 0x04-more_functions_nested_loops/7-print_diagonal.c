761b05185fca4e02512f7c73c457dff6e0e017ac
Code@Home <bedada@gmail.com>
2022-04-14 13:35:59 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,6 @@
 /**
  * print_diagonal - prints diagonal.
  * @n: The first number
- *
  * Return: 0
  */
 
@@ -18,10 +17,8 @@ void print_diagonal(int n)
 			for (space = 0; space < len; space++)
 				_putchar(' ');
 			_putchar('\\');
-
 			if (len == n-1)
 				continue;
-
 			_putchar('\n');
 		}
 	}
