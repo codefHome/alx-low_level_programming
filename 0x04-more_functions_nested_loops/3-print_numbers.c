21c8eeae2d364274bd753b44b83f2ca3facc497b
Code@Home <bedada@gmail.com>
2022-04-14 11:59:08 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 to 9.
+ * @d: The first number
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9' i++)
+		_putchar((i % 10) + '0');
+	_putchar('\n');
+}
