08f3c613d20184454a96dab4ff315e05a6ba9678
Code@Home <bedada@gmail.com>
2022-04-20 05:20:36 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,15 @@ void print_rev(char *s)
 {
 	int len = 0, index;
 
-	while (s[index++])
+	while (*s != '\0')
+	{
 		len++;
+		index++;
+	}
+	s--;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	for (index = 0; index < len; index++, s--)
+		_putchar(*s);
 
 	_putchar('\n');
 }
