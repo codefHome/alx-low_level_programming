06349d896a1db73e4268050bdd0b728b7fe0183c
Code@Home <bedada@gmail.com>
2022-04-20 03:22:12 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,12 @@
 /**
  * _strlen - Returns the length of a string.
  * @x: The string to get the length of
+ * Return: int
  */
 int _strlen(char *x)
 {
 	int count;
+
 	while (*x != '\0')
 	{
 		count++;
