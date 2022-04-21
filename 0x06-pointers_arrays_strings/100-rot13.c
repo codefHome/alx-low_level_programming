1a23b87c05d89d7dc4a3ee821661265a2eab6274
Code@Home <bedada@gmail.com>
2022-04-21 13:49:58 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,13 +10,14 @@ char *rot13(char *k)
 {
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
 	int i = 0;
 	int j = 0;
 
 	while (k[i] != '\0')
 	{
 		for (j = 0; a[j] != '\0' && k[i] != a[j]; j++)
-		;
+			;
 		if (j < 52)
 		{
 			k[i] = b[j];
