d0f2f584b463293341d9f8c2925ed3ebd8a890c2
Code@Home <bedada@gmail.com>
2022-04-21 11:07:53 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int length = 0;
-	int length1 = 0;
-	int x;
+	int index = 0, dest_len = 0;
 	
-	while (*dest != '\0')
-	{
-		dest++;
-		length++;
-	}
-	length1 = length;
-	for (x = 0; x < n && *src; dest++, src++, length++, x++)
-	{
-		*dest = *src;
-	}
-	if (length1 + n >= length)
-		*(dest + length) = '\0';
-	dest = dest - length;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
