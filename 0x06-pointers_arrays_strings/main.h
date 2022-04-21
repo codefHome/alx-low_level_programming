022ec6223c08aff410638b5dcfecdf56255e3d8f
Code@Home <bedada@gmail.com>
2022-04-21 12:50:51 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * Desc: Header file containing prototypes for all functions
  *	used in the 0x06-pointers_arrays_strings directory.
  */
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
