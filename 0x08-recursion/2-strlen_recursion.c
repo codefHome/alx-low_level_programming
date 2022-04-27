4ad6e28dfab9065d5c929fbe4cd09f204f585b13
Bedada <bedada6@gmail.com>
2022-04-27 09:17:45 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int count = s.length;
+	int count = 0;
 
 	if (*s != '\0')
 	{
 	
-		_strlen_recursion(s + 1);
-		count++;
+		count += _strlen_recursion(s + 1);
+		
 	}
 	return (count);
 }
