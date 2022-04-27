18ba4f268b0934a9841a82e376d9618f0ff30ff5
Bedada <bedada6@gmail.com>
2022-04-27 07:03:15 -0700

    0-puts_recursions.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * 0-puts_recursion - prints strings to the std
+ * _puts_recursion - prints strings to the std
  * @s: string
  * Return: string on Success
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_putchar(*s + 1);
