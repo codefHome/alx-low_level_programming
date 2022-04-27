67c4e638245c183d29c5013393a245f577d7aee5
Bedada <bedada6@gmail.com>
2022-04-27 08:57:09 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen_recursion - prints length of the string
+ * @s: string
+ * Retrun: string length on sucess
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count=0;
+
+	if (*s)
+	{
+		cout++;
+	}
+	return count;
+}
