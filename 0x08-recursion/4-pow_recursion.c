091fb41a7275205b10c44be2935b2b6ed73fa434
Bedada <bedada6@gmail.com>
2022-04-27 10:01:00 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print power of the integer number
+ * @x: base number
+ * @y: power number
+ * Return: power of the number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return x * _pow_recursion(x, y - 1);
+}
