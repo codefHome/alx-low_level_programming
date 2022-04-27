bc532f233b47a0f39b75ad9a8f66769e32055858
Bedada <bedada6@gmail.com>
2022-04-27 08:52:38 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion prints string in reverse order
+ * _print_rev_recursion - prints string in reverse order
  * @s: string
  * Return: string on success
  */
@@ -10,7 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
