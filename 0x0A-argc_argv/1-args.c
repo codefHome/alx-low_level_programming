eb012c3f8c0193cc225e36599dbb2cb703a36bca
Bedada <bedada6@gmail.com>
2022-04-29 10:57:17 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: name of arguments
+ * Return: always return 0 
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
