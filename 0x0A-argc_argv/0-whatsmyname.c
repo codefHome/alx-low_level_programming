7543240dab92b41b4c217236364bc27dca898d68
Bedada <bedada6@gmail.com>
2022-04-29 10:50:21 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,10 @@
  * Return: 0 always
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("Prints Program Name\n");
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
