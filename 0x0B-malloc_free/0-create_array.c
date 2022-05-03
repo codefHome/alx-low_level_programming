9b95bb5b82347df4741aeb05dfd2163d52d4113d
Bedada <bedada6@gmail.com>
2022-05-03 04:27:45 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 	if (size == 0)
 		return (NULL);
-	if (arr == NULL)
-		return (NULL);
+
 	return (arr);
 }
 
