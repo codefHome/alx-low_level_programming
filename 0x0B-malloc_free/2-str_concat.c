afc1449efede95ceb99a75945f879d2a39a5696b
Bedada <bedada6@gmail.com>
2022-05-03 07:55:07 -0700

    2-str-concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i, j, slen, slen1;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i]; i++)
 		slen++;
 	for (j = 0; s1[j]; j++)
@@ -21,13 +26,12 @@ char *str_concat(char *s1, char *s2)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; s1[i]; i++)
-		str[i] = s1[i];
-	for (j = slen; s2[j]; j++)
+		str[index++] = s1[i];
+	for (j = 0; s2[j]; j++)
 	{
-		int z = 0;
-		str[j]= s2[z];
-		z++;
+		str[index++]= s2[j];
 	}
+	str[index] = '\0';
 	return (str);
 }
 
