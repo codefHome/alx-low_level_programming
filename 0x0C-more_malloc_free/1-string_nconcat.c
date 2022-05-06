4a4168ff460ea89f57652cafd5922f5248ecca8f
Bedada <bedada6@gmail.com>
2022-05-06 07:20:27 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate two string
+ * @s1: string 
+ * @s2: string
+ * Return: concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char newstr[];
+	int i, j, ii, jj, index;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i]; i++)
+		ii++;
+	for (j = 0; s2[j]; j++)
+		jj++;
+
+	newstr = malloc((sizeof(char) * jj) + (sizeof(char) * ii));
+	if (newstr == NULL)
+		return (NULL);
+	for (i = 0; i < ii ; i++)
+	{
+		newstr[index++] = s1[i];
+	}
+	for (j = 0; j < jj; j++)
+	{
+		newstr[index++] = s2[j];
+	}
+return (newstr);
+}
+
