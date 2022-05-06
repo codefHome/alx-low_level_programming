4027f5d0f709f2103c1da9e3c9c995e27bf10347
Bedada <bedada6@gmail.com>
2022-05-06 09:32:31 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * max);
+	arr = malloc(sizeof(int) * ((max - min) + 1));
 	if (arr == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
