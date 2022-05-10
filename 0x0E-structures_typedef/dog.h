1b4e23e77e0bed305d8a45855e4dc3fc73d2ff15
Bedada <bedada6@gmail.com>
2022-05-10 06:23:41 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_h
 #define DOG_h
 
+/**
+ * struct dog - create dog data type
+ * @name: string
+ * @age: float
+ * owner: string
+ */
+
 typedef struct dog {
 	char *name;
 	float age;
