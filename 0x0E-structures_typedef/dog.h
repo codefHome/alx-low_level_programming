467d2ff73b561b1903a202c5da3106c65772e442
Bedada <bedada6@gmail.com>
2022-05-10 06:26:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,10 @@
  * owner: string
  */
 
-typedef struct dog {
+struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-int _strlen(char *s);
 
-#endif
